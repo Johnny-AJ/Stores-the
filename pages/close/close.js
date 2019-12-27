9540a8c548ce1f7e0d20c7c5d994b7c7975ab9e3
高桥老湿 <gaoqiaolaoshi@qq.com>
2019-12-27 16:12:15 +0800

    完成页面

--- pages/close/close.js
+++ pages/close/close.js
@@ -1,66 +1,19 @@
-// pages/close/close.js
+// pages/demo6/demo6.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    name: 'name1'
+    isshow: false //默认闭合
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
+  toggle(e) { //图片点击事件
+    console.log(e)
+    // this.isshow =!this.isshow;
+    this.setData({
+      isshow: !this.data.isshow
+    })
+    // this.$apply();
+    console.log(this.data.isshow)
   }
 })
