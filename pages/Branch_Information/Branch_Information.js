09dd244e92e068f7b33ac2a45417b020db114ec7
llc4399 <LLC4399@163.com>
2020-01-07 16:09:23 +0800

    1.21

--- pages/Branch_Information/Branch_Information.js
+++ pages/Branch_Information/Branch_Information.js
@@ -1,4 +1,8 @@
 // pages/Branch_Information/Branch_Information.js
+
+let confing =require('../../utils/config.js');
+let http =require('../../utils/http.js')
+
 Page({
 
   /**
@@ -7,14 +11,14 @@ Page({
   data: {
     storeName: '', //门店
     linkman: '', //联系人
-    phone: '', //联系号码
+    phone:'', //联系号码
     addre: '', //地址
     photo: '', //上传资质
     province: '', //省
     city: '', //市
     region: "", //区
-
-    uploaderList: [], //图片列表
+    pics: [], //本地存储路径
+    images: [], //图片列表
     uploaderNum: 0, //图片数量
     showUpload: true, //超过图片数量  默认9张
   },
@@ -62,8 +66,151 @@ Page({
   },
   // 提交
   handleTabs(e) {
-    // console.log(e, '提交')
-    let self = this
+    let self = this;
+    var pics = self.data.pics; //本地图片
+    if (pics.length > 0) {
+      var imgs = [];
+      pics.forEach(i => {
+        wx.uploadFile({
+          url: confing.domain + '/api/file/save', //上传图片
+          filePath: i,
+          name: 'file',
+          formData: {
+            'user': 'test'
+          },
+          success(res) {
+            imgs.push(JSON.parse(res.data).msg)
+            self.setData({
+              images: imgs
+            }, () => {
+              self.save()
+            })
+          }
+        })
+      })
+    } else {
+      self.save1()
+    }
+   
+  },
+  chooseImg: function (e) { //上传图片
+    var that = this,
+      pics = this.data.pics;
+
+    if (pics.length < 9) {
+      wx.chooseImage({
+        count: 9, // 最多可以选择的图片张数，默认9
+        sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有
+        success: function (res) {
+
+          // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+          var tempFilePaths = res.tempFilePaths;
+          // wx.showToast({
+          //   title: '正在上传...',
+          //   icon: 'loading',
+          //   mask: true,
+          //   duration: 10000
+          // });
+          for (var i = 0; i < tempFilePaths.length; i++) {
+            pics.push(tempFilePaths[i]);
+          }
+
+          that.setData({
+            pics: pics
+          })
+        },
+      });
+    } else {
+      wx.showToast({
+        title: '最多上传9张图片',
+        icon: 'none',
+        duration: 3000
+      });
+
+    }
+  },
+  deleteImg: function (e) {
+    var that = this;
+    var pics = this.data.pics;
+    var index = e.currentTarget.dataset.index;
+    pics.splice(index, 1);
+    console.log(pics)
+    this.setData({
+      pics: pics,
+    })
+  },
+
+  save(){
+
+    console.log('save')
+    var self =this;
+  let prams={
+    storeName: self.data.storeName,
+    linkman: self.data.linkman,
+    phone: self.data.phone,
+    province: self.data.province[0],
+    city: self.data.city[1],
+    region: self.data.region[2],
+    imgs: self.data.uploaderList,
+    address: self.data.address
+  }
+    http.postRequest('/api/store/info/save',prams,function(res){
+      console.log(res, "用户信息")
+      if (res.data.code == 0) {
+        wx.showToast({
+          title: '信息提交中',
+          icon: 'none',
+          duration: 1500
+        })
+        wx.navigateTo({
+          url: "/pages/audit/audit"
+        })
+      } else {
+        wx.showToast({
+          title: res.data.msg,
+          icon: 'none',
+          duration: 1500
+        })
+      }
+    })
+    // wx.request({
+    //   url: 'http://192.168.2.98:9098/api/store/info/save', //完善用户信息
+    //   method: "POST",
+    //   data: {
+    //     storeName: self.data.storeName,
+    //     linkman: self.data.linkman,
+    //     phone: self.data.phone,
+    //     province: self.data.province[0],
+    //     city: self.data.city[1],
+    //     region: self.data.region[2],
+    //     imgs: self.data.uploaderList,
+    //     address: self.data.address
+    //   },
+    //   success: res => {
+    //     console.log(res, "用户信息")
+    //     if (res.data.code == 0) {
+    //       wx.showToast({
+    //         title: '信息提交中',
+    //         icon: 'none',
+    //         duration: 1500
+    //       })
+    //       wx.navigateTo({
+    //         url: "/pages/audit/audit"
+    //       })
+    //     } else {
+    //       wx.showToast({
+    //         title: res.data.msg,
+    //         icon: 'none',
+    //         duration: 1500
+    //       })
+    //     }
+    //   }
+    // })
+  },
+  save1() {
+    console.log('save1')
+    var self = this;
     wx.request({
       url: 'http://192.168.2.98:9098/api/store/info/save', //完善用户信息
       method: "POST",
@@ -98,59 +245,6 @@ Page({
       }
     })
   },
-  // 删除图片
-  clearImg: function(e) {
-    var nowList = []; //新数据
-    var uploaderList = this.data.uploaderList; //原数据
-
-    for (let i = 0; i < uploaderList.length; i++) {
-      if (i == e.currentTarget.dataset.index) {
-        continue;
-      } else {
-        nowList.push(uploaderList[i])
-      }
-    }
-    this.setData({
-      uploaderNum: this.data.uploaderNum - 1,
-      uploaderList: nowList,
-      showUpload: true
-    })
-  },
-  //展示图片
-  showImg: function(e) {
-    var that = this;
-    wx.previewImage({
-      urls: that.data.uploaderList,
-      current: that.data.uploaderList[e.currentTarget.dataset.index]
-    })
-  },
-  //上传图片
-  upload: function(e) {
-    var that = this;
-    wx.chooseImage({
-      count: 9 - that.data.uploaderNum, // 默认9
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-      success: function(res) {
-        console.log(res)
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        let tempFilePaths = res.tempFilePaths;
-        let uploaderList = that.data.uploaderList.concat(tempFilePaths);
-
-        if (uploaderList.length == 9) {
-          that.setData({
-            showUpload: false
-          })
-        }
-        that.setData({
-          uploaderList: uploaderList,
-          uploaderNum: uploaderList.length,
-        })
-
-        console.log(that.data.uploaderList, 'uploaderList')
-      }
-    })
-  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -176,38 +270,5 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  }
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,7 @@
 // pages/login/login.js
+
+
+let http = require('../../utils/http.js')
 Page({
 
   /**
@@ -7,7 +10,7 @@ Page({
   data: {
     mobile: '', //手机号    13189413198  13143698421  17344573220   17817891541
     code: '', //验证码
-    type: 2 //验证码类型  0注册,1重置密码,2登录,3解绑手机号,4绑定手机号
+    type: 0 //验证码类型  0注册,1重置密码,2登录,3解绑手机号,4绑定手机号
   },
   // 手机号
   handNumber(e) {
@@ -28,97 +31,134 @@ Page({
   },
   // 获取验证码
   handleVerification(e) {
+
+
+
+
     let self = this
     if (self.data.mobile) {
-      wx.request({
-        url: 'http://192.168.2.98:9098/api/store/generateCode',
-        method: 'POST',
-        dataType: 'application/json',
-        data: {
-          mobile: self.data.mobile,
-          type: self.data.type
-        },
-        success: res => {
-          console.log(res, "获取验证码")
-          if (res.data.code == 0) {
-            wx.showToast({
-              title: res.data.msg,
-              icon: 'none',
-              duration: 1500
-            })
+      http.getRequest('/api/store/findByPhone', {
+        phone: self.data.mobile
+      }, function(res) {
+        console.log(res, 'findByPhone');
+
+        if (res.data.data) { //true注册  false登陆
+
+          self.setData({
+            type: 0
+          })
+          let prams1 = {
+            mobile: self.data.mobile,
+            type: 0
           }
+          http.postRequest('/api/store/generateCode', prams1, function(res) {
+            console.log(res, "获取验证码")
+            if (res.data.code == 0) {
+
+              console.log(res,'generateCode1')
+              wx.showToast({
+
+                title: '正在获取验证码',
+                icon: 'none',
+                duration: 1500
+              })
+            }
+          })
+        } else {
+
+          self.setData({
+            type:2
+          })
+          let prams2 = {
+            mobile: self.data.mobile,
+            type: 2
+          }
+          http.postRequest('/api/store/generateCode', prams2, function (res) {
+            console.log(res, "获取验证码")
+            if (res.data.code == 0) {
+
+            console.log(res, 'generateCode2')
+              wx.showToast({
+                title: '正在获取验证码',
+                icon: 'none',
+                duration: 1500
+              })
+            }
+          })
         }
+
       })
+      // let prams = {
+      //   mobile: self.data.mobile,
+      //   type: self.data.type
+      // }
+      // http.postRequest('/api/store/generateCode', prams, function(res) {
+      //   console.log(res, "获取验证码")
+      //   if (res.data.code == 0) {
+      //     wx.showToast({
+      //       title: '正在获取验证码',
+      //       icon: 'none',
+      //       duration: 1500
+      //     })
+      //   }
+      // })
+      // wx.request({
+      //   url: 'http://192.168.2.98:9098/api/store/generateCode',
+      //   method: 'POST',
+      //   dataType: 'application/json',
+      //   data: {
+      //     mobile: self.data.mobile,
+      //     type: self.data.type
+      //   },
+      //   success: res => {
+      //     console.log(res, "获取验证码")
+      //     if (res.data.code == 0) {
+      //       wx.showToast({
+      //         title: res.data.msg,
+      //         icon: 'none',
+      //         duration: 1500
+      //       })
+      //     }
+      //   }
+      // })
     }
   },
   // 注册/登陆
   handleRegister(e) {
     let self = this
-    wx.request({
-      url: 'http://192.168.2.98:9098/api/store/findByPhone', //判断手机号是否注册过
-      data: {
-        phone: self.data.mobile
-      },
-      success: res => {
-        console.log(res.data.data, 'res1')
-        if (res.data.data == true) { //true注册  false登陆
-          wx.request({
-            url: 'http://192.168.2.98:9098/api/store/register', //注册
-            method: "POST",
-            dataType: 'application/json',
-            data: {
-              mobile: self.data.mobile,
-              code: self.data.code,
-              type: 0
-            },
-            success: res => {
-              console.log(res, "注册")
-              if (res.data.code == 0) { //status：0待审核,1通过,2不通过,3待补充信息
-                wx.showToast({
-                  title: '注册中',
-                  icon: 'none',
-                  duration: 1500
-                })
-                wx.navigateTo({
-                  url: "/pages/Branch_Information/Branch_Information?phone=" + self.data.mobile,
-                })
-              } else {
-                wx.showToast({
-                  title: res.data.msg,
-                  icon: 'none',
-                  duration: 1500
-                })
-              }
-            }
-          })
-        } else {
-          wx.request({
-            url: 'http://192.168.2.98:9098/api/store/login', //登陆
-            method: "POST",
-            dataType: 'application/json',
-            data: {
-              code: self.data.code,
-              mobile: self.data.mobile,
-              type: 2
-            },
-            success: res => {
-              if (res.data.code == 0) {
-                console.log(res, "res2")
-                wx.navigateTo({
-                  url: "/pages/home/home"
-                })
-              } else {
-                wx.showToast({
-                  title: res.data.msg,
-                  icon: 'none',
-                  duration: 1500
-                })
-              }
-            }
+
+    if (self.data.type==0) { //true注册  false登陆
+      let prams2 = {
+        code: self.data.code,
+        mobile: self.data.mobile,
+        type: 0
+      }
+      http.postRequest('/api/store/register', prams2, function (res) {
+
+        if (res.data.data.status == 3) {
+          wx.navigateTo({
+            url: '/pages/Branch_Information/Branch_Information?phone=' + self.data.mobile,
           })
         }
+        console.log(res, 'register')
+      })
+    } else {
+      let prams1 = {
+        code: self.data.code,
+        mobile: self.data.mobile,
+        type: 2
       }
-    })
+      http.postRequest('/api/store/login', prams1, function (res) {
+        console.log(res, 'login')
+
+        if(res.data.data.status==3){
+          wx.navigateTo({
+            url: '/pages/Branch_Information/Branch_Information?phone=' + self.data.mobile,
+          })
+        }
+      })
+    }
+
   },
   /**
    * 生命周期函数--监听页面加载
